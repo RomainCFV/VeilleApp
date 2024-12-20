1e4783427155e47b86ab4bc9e93d5f408c15fe60
RomainCFV <rengokuryu.vortex@gmail.com>
2024-12-20 17:50:46 +0100

    [UPDATE] Updated models and pages

--- Models/Veille.cs
+++ Models/Veille.cs
@@ -1,15 +1,24 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
 
 namespace VeilleApp.Model
 {
+    [Index(nameof(Guid), IsUnique = true)]
     public class Veille
     {
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+        [Required]
         public string Title { get; set; } = string.Empty;
         public string Content { get; set; } = string.Empty;
+        [Required]
         public string Link { get; set; } = string.Empty;
+        [Required]
         public string Publisher { get; set; } = string.Empty;
         public DateTime PublishTime { get; set; }
+        [Required]
+        public string Guid { get; set; }
     }
 }
